8a097fe19a0def3532343957f18d75f964e10813
hjjung990927 <hjjung990927@gmail.com>
2026-01-05 15:38:26 +0900

    update ai api key

--- src/main/resources/static/js/community/post/event.js
+++ src/main/resources/static/js/community/post/event.js
@@ -189,7 +189,7 @@ document.body.addEventListener("click", async (e) => {
         const files = writeFiles.files;
 
         // 게시글 작성 시 욕설 확인
-        const response = await fetch(`https://started-speeches-grip-natural.trycloudflare.com/api/community/word-check`, {
+        const response = await fetch(`https://blair-limit-allergy-tribunal.trycloudflare.com/api/community/word-check`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
